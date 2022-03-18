fb5993ac0a5f6f8348417917469b9ebc3f25d48c
Eduardo <76177427+Edubeth@users.noreply.github.com>
2022-03-18 17:20:19 -0600

    Add files via upload

--- src/main/java/Dog.java
+++ src/main/java/Dog.java
@@ -1,2 +1,16 @@
 public class Dog {
+
+    String Nombre;
+    int Edad;
+
+    public Dog(String Nombre, int Edad){
+
+        this.Edad=Edad;
+        this.Nombre=Nombre;
+    }
+
+    public void print(){
+        System.out.print("Nombre " + this.Nombre + " Edad " + this.Edad);
+    }
+}
 }
